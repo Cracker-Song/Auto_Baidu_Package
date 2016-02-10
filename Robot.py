2e0fe2743f6661e06484c48cfb3e4717418c0b65
Cracker-Song <857153944@qq.com>
2016-02-11 01:30:45 +0800

    add more api

--- Robot.py
+++ Robot.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 import json
 import requests
-headers = {
-    'apikey':'fc9e6ac7e5c99cda6eea29ee8fa59f00'
-}
 
 
-def robot(content):
-    key = '879a6cb3afb84dbf4fc84a1df2ab7319'
-    robot_url = 'http://apis.baidu.com/turing/turing/turing?key=%s&info=%s' % (key, content)
-    robot_request = requests.get(robot_url, headers=headers)
+def robot(content, userid):
+    key = 'e26b51b1b28c56f50612cee49bbba898'
+    robot_url = 'http://www.tuling123.com/openapi/api?key=%s&info=%s&userid=%s' % (key, content, userid)
+    robot_request = requests.post(robot_url)#, headers=headers)
     respond_robot = json.loads(robot_request.text)
-    respond = respond_robot['text']
+    code = respond_robot['code']
+    if code==100000:
+        respond = respond_robot['text']
+    elif code==200000:
+        respond = '%s  %s ' % (respond_robot['text'], respond_robot['url'])
+    elif code==302000:
+        respond = '%s\n' % respond_robot['text']
+        #print respond_robot
+        for news in respond_robot['list']:
+            respond += u'标题:%s \n来源:%s \n缩略图:%s \n详细:%s \n' % (news['article'], news['source'], news['icon'], news['detailurl'])
+    elif code==308000:
+        respond = '%s\n' % respond_robot['text']
+        #print respond_robot
+        for cook in respond_robot['list']:
+            #print type(cook['info'])
+            respond += u'菜名:%s \n图片:%s \n做法:%s \n详细:%s\n' % (cook['name'], cook['icon'], cook['info'], cook['detailurl'])
+    else:
+        respond = code
     return respond
+print robot('我想看新闻', 'test')
